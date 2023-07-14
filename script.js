1ff25ee6e9aa9440a76eaa865f0a23f3a205dc72
VladPryima <kassel0088@gmail.com>
2023-07-14 15:25:30 +0300

    responsive and adaptive for resolution from 300px

--- script.js
+++ script.js
@@ -4,18 +4,26 @@ let lineOne = document.querySelector(".line-one");
 let lineTwo = document.querySelector(".line-two");
 let lineThree = document.querySelector(".line-three");
 
-/*Что нужно сделать по клику: 
-Линии должны стать крестом внутри burger - два span, один из которых отключаем.
-добавить класс line--one - two - three к трем span - выглядит муторно.
-*/
 
 function openMenu () {
     dropdown.classList.toggle("visible");
 
     lineOne.classList.toggle("line-one--rotate");
     lineTwo.classList.toggle("line-two--hide");
-    lineThree.classList.toggle("line-three--rotate");
-    
+    lineThree.classList.toggle("line-three--rotate");   
 }
 
 burger.addEventListener("click", openMenu);
+
+
+//If screen width > 768px - remove classes from burger and dropdown
+function checkSreenWidth () {
+    if (window.innerWidth > 768) {
+        dropdown.classList.remove("visible");
+
+        lineOne.classList.remove("line-one--rotate");
+        lineTwo.classList.remove("line-two--hide");
+        lineThree.classList.remove("line-three--rotate");
+    } 
+}
+window.addEventListener("resize", checkSreenWidth);
